69974f813d72de1c58a3b35f60bf9a0695c219ed
Luis Henrique F Ramires <luishenriquefr@outlook.com>
2023-11-02 21:41:37 -0300

    added one more variable and a comment

--- file_one.py
+++ file_one.py
@@ -1,3 +1,7 @@
 var = 12
 
 print(var)
+
+string = 'now I changed the code!'
+# just forgot to add a comment to save that var = 12 and the print was the first modification
+# to the repository, and now i'm adding this comment and the var string
